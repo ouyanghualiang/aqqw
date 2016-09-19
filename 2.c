e7e468cda4e9021547fcead81cf9f1ebcf3cc665
aqqw_ouyang <hualiang0305@foxmail.com>
2016-09-19 15:04:24 +0800

    touch 2.c file

---
+++ 2.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 2.c
+	> Author: aqqw_ouyang
+	> Mail: hualiang0305@foxmail.com 
+	> Created Time: 2016年09月19日 星期一 15时00分17秒
+ ************************************************************************/
+
+#include<stdio.h>
+void son_fun(int a)
+
+{
+	int b = a;
+	printf("%d\t",a * b);
+	printf("%d\n",a);
+	return ;
+}
